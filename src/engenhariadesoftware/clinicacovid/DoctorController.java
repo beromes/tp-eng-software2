b63ef7f7ef94a47a4f82222567c49de77fb54742
Fernando Moreira <fernandoepm.pro@gmail.com>
2022-05-31 11:06:55 -0300

    Refatorando a função handleDoctor. Primeiro, extraímos a função para uma nova classe DoctorController (extract class) e na própria handleDoctor foi realizado uma extração de métodos: foi criado uma nova função para imprimir o menu na tela e outra para imprimir as consultas marcadas (extract method). Estas alterações têm como objetivo evitar duplicação de código e melhorar a organização.

---
+++ src/engenhariadesoftware/clinicacovid/DoctorController.java
@@ -0,0 +1,79 @@
+package engenhariadesoftware.clinicacovid;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Calendar;
+import java.util.Scanner;
+
+import engenhariadesoftware.clinicacovid.DataController;
+import engenhariadesoftware.clinicacovid.GetUserInterface;
+import engenhariadesoftware.clinicacovid.model.Doctor;
+import engenhariadesoftware.clinicacovid.model.Appointment;
+import engenhariadesoftware.clinicacovid.model.Patient;
+import engenhariadesoftware.clinicacovid.Main;
+
+public class DoctorController extends ModelController<Doctor>  {
+
+  DoctorController(DataController dataController, Scanner in) {
+    this.dataController = dataController;
+    this.in = in; 
+  }
+
+  @Override
+  protected Doctor getUser(String username) {
+    return this.dataController.getDoctor(username);
+  }
+
+  public void handleDoctor() {
+
+    Doctor doctor = getUser();
+
+    if (doctor == null) {
+			return;
+		}
+
+		handleDocLoop: 
+    while(true) {	
+
+      printDoctorMenu(doctor.getName());
+			
+			int option = in.nextInt();	
+			switch(option) {
+				case 1:
+					Calendar cal = doctor.getCalendar();
+					List<Appointment> aps = cal.getAppointments();
+					if(!aps.isEmpty() ) {
+						printScheduledAppointments(aps);
+					} else {
+						println("Não há consultas marcadas\n");
+					}
+					break;
+				case 2:
+					controller.goToMenu();
+					break handleDocLoop;
+				default:
+					println("Opção inválida");
+					continue;
+			}
+			
+		}
+	}
+
+  private void printDoctorMenu(String name) {
+    println("Bem vindo(a) " + name + "!");
+		println("1. Ver Agenda");
+		println("2. Voltar");
+  }
+
+  private printScheduledAppointments(List<Appointment> aps) {
+    println("Consultas Marcadas: ");
+    for(Appointment ap : aps) {
+      Patient pat = ap.getPatient();
+      Date date = ap.getAppointmentDate();
+      println(date.toLocaleString() + ": " 
+          + pat.getName() + " (" + pat.getPhone() + ")");
+    }
+    println("");
+  }
+
+}
--- src/engenhariadesoftware/clinicacovid/Main.java
+++ src/engenhariadesoftware/clinicacovid/Main.java
@@ -51,7 +51,7 @@ public class Main {
 	private static void handleMode(Mode mode) {
 		switch(mode) {
 			case Doctor:
-				handleDoctor();
+				DoctorController.handleDoctor();
 				break;
 			case Nurse:
 				NurseController.handleNurse(controller);
@@ -84,52 +84,6 @@ public class Main {
 				return user;
 		}
 		return null;
-	}			
-	
-	private static void handleDoctor() {
-		Doctor doctor = null;
-		handleDocLoop: while(true) {		
-			if(doctor == null) {
-				doctor = getUser(new GetUserInterface<Doctor>() {
-					@Override
-					public Doctor getUser(String username) {
-						return controller.getDoctor(username);
-					}
-				});
-				if(doctor == null) break;
-			}
-			
-			println("Bem vindo(a) " + doctor.getName() + "!");
-			println("1. Ver Agenda");
-			println("2. Voltar");
-			
-			int option = in.nextInt();	
-			switch(option) {
-				case 1:
-					Calendar cal = doctor.getCalendar();
-					List<Appointment> aps = cal.getAppointments();
-					if(!aps.isEmpty() ) {
-						println("Consultas Marcadas: ");
-						for(Appointment ap : aps) {
-							Patient pat = ap.getPatient();
-							Date date = ap.getAppointmentDate();
-							println(date.toLocaleString() + ": " 
-									+ pat.getName() + " (" + pat.getPhone() + ")");
-						}
-						println("");
-					}else {
-						println("Não há consultas marcadas\n");
-					}
-					break;
-				case 2:
-					controller.goToMenu();
-					break handleDocLoop;
-				default:
-					println("Opção inválida");
-					continue;
-			}
-			
-		}
 	}
 	
 	private static void println(String str) {
