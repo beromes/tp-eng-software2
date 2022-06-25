053905a22dd34b5ab5f5e4164e68e4c7e7090577
Bernardo Nunes <nunesbernardo2000@gmail.com>
2022-06-25 16:20:00 -0300

    test workflow

---
+++ src/test/engenhariadesoftware/clinicacovid/unitTests/Teste.java
@@ -0,0 +1,14 @@
+package test.engenhariadesoftware.clinicacovid.unitTests;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Teste {
+
+    @Test
+    public void test() {
+        assertTrue(1 == 1);
+    }
+}
